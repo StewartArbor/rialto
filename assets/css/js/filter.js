bafd58b9a297c6e19d0cd7cc6a23644475511f8f
Stewart Arbor <treemeliority@gmail.com>
2026-02-17 23:09:05 +0500

    something

---
+++ assets/css/js/filter.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const filterBtn = document.getElementById("filterBtn");
+
+  if (!filterBtn) return;
+
+  filterBtn.addEventListener("click", () => {
+    alert("Filters coming soon 👀");
+  });
+
+});
